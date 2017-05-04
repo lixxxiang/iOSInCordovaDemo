d5d5765d02af893313b9518ebab03640b544d9bf
lixxxiang <13051744716@163.com>
2017-05-04 16:44:47 +0800

    Initial Commit

---
+++ iOSInCordovaDemoTests/iOSInCordovaDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  iOSInCordovaDemoTests.m
+//  iOSInCordovaDemoTests
+//
+//  Created by yyfwptz on 2017/5/4.
+//  Copyright © 2017年 yyfwptz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOSInCordovaDemoTests : XCTestCase
+
+@end
+
+@implementation iOSInCordovaDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
